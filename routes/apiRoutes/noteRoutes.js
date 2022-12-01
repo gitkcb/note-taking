e1cf7e77ac1963378f944fb7af03de657c544391
gitkcb <kcharlesbell@gmail.com>
2022-12-01 17:19:41 -0500

    corrected routing

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,25 +1,36 @@
 const noteRoutes = require('express').Router();
 const fs = require('fs');
+const uuid = require('../../helpers/uuid');
+
+
 
 noteRoutes.get('/notes', (req, res) => {
-    const results = notes;
-    res.json(results);
+    fs.readFile('db/db.json', (err, data) => {
+        let results = JSON.parse(data);
+        res.json(results);
+
+    })
+   
 });
 
 noteRoutes.post('/notes', (req, res) => {
-    fs.readFile('/db/db.json', (err,data) => {
+    fs.readFile('db/db.json', (err,data) => {
         let notes = JSON.parse(data);
         const note = {
             title: req.body.title,
             text: req.body.text,
+            id: uuid(),
         };
     console.log(note);
-    res.json(note);
+    
 
     notes.push(note);
     console.log(notes);
 
-    fs.writeFile('/db/db.json', JSON.stringify(notes, null, 2));
+    fs.writeFile('db/db.json', JSON.stringify(notes),(data, err) => {
+        console.log(data);
+        res.json(data);
+    });
     });
 });
 module.exports = noteRoutes;
--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 const express = require('express');
 
-
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require("./routes/apiRoutes/noteRoutes.js");
 const htmlRoutes = require('./routes/htmlRoutes');
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
 app.use(express.static('public'));
+app.use(express.json());
 
+app.use(express.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-
-
+module.exports = app;
 
 app.listen(PORT, () =>
 console.log(`Listening at http://localhost:${PORT}`));
